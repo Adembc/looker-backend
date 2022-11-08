2e7671aa1b44b71f2900d13e063b2107c55f6250
Adem Baccara <71262172+Adembc@users.noreply.github.com>
2022-11-08 16:18:27 +0100

    Delete dist directory

--- dist/helper/ferzfr.js
+++
@@ -1,6 +0,0 @@
-(category &&
-    category
-        .split(",")
-        .filter((ele) => Types.ObjectId.isValid(ele))
-        .map((ele) => new Types.ObjectId(ele))) ||
-    [];
--- dist/helper/filePath.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function filePath(path) {
-    if (!path)
-        return undefined;
-    return process.env.SERVER_URL + path.slice("public/".length);
-}
-exports.default = filePath;
